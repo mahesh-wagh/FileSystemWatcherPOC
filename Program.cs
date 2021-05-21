7de288fef715176abd5ce2dda2197f5541d7d83c
mahesh-wagh <maheshbwagh@gmail.com>
2021-05-21 17:35:12 +0530

    Using PhysicalFileWatcher

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 
 namespace FileSystemWatcherPOC
@@ -6,23 +10,60 @@ namespace FileSystemWatcherPOC
     class Program
     {
         private static FileSystemWatcher _fileSystemWatcher;
+
+        private static IChangeToken _fileChangeToken;
+        private static PhysicalFileProvider _fileProvider;
+        private static readonly ConcurrentDictionary<string, DateTime> _files = new ConcurrentDictionary<string, DateTime>();
+        private static string _filePath= "/another";
+        public static void DoWork()
+        {
+            _fileProvider = new PhysicalFileProvider(_filePath); // e.g. C:\temp
+            WatchForFileChanges();
+        }
+
+        private static void WatchForFileChanges()
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(_filePath, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                if (_files.TryGetValue(file, out DateTime existingTime))
+                {
+                    _files.TryUpdate(file, File.GetLastWriteTime(file), existingTime);
+                }
+                else
+                {
+                    if (File.Exists(file))
+                    {
+                        _files.TryAdd(file, File.GetLastWriteTime(file));
+                    }
+                }
+            }
+            _fileChangeToken = _fileProvider.Watch("**/*.*");
+            _fileChangeToken.RegisterChangeCallback(Notify, default);
+        }
         static void Main(string[] args)
         {
             try
             {
-                var filePath = "/myvol/";
-                Console.WriteLine(System.Environment.CurrentDirectory);
-                var _fileSystemWatcher = new FileSystemWatcher(filePath)
-                {
-                    Filter = "*.txt",
-                    NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size,
-                    EnableRaisingEvents = true,
-                    IncludeSubdirectories = true
-                };
-               
-                _fileSystemWatcher.Changed += Notify;
-                _fileSystemWatcher.Created += Notify;
-                _fileSystemWatcher.Deleted += Notify;
+
+                DoWork();
+
+
+
+
+                //var filePath = "/app/another";
+                //Console.WriteLine(System.Environment.CurrentDirectory);
+                //var _fileSystemWatcher = new FileSystemWatcher(filePath)
+                //{
+                //    Filter = "*.txt",
+                //    NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size,
+                //    EnableRaisingEvents = true,
+                //    IncludeSubdirectories = true
+                //};
+
+                //_fileSystemWatcher.Changed += Notify;
+                //_fileSystemWatcher.Created += Notify;
+                //_fileSystemWatcher.Deleted += Notify;
 
 
 
@@ -40,6 +81,12 @@ namespace FileSystemWatcherPOC
         private static void Notify(object sender,FileSystemEventArgs e)
         {
             Console.WriteLine($"{e.FullPath}-{e.ChangeType}");
+        }
+
+        private static void Notify(object state)
+        {
+            Console.WriteLine("File activity detected.");
+            WatchForFileChanges();
         }
     }
 }
